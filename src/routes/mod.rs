fbe394a62bca9b586a030394b4f91390917166e6
alendavies <alendavies10@gmail.com>
2025-12-10 12:02:49 -0300

    refactor: organize route configuration for improved readability

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,3 +4,43 @@ pub mod ground_stations;
 pub mod jobs;
 pub mod satellites;
 pub mod telemetry;
+
+use actix_web::web;
+
+use self::{
+    config::get_config,
+    control::send_command,
+    ground_stations::{
+        create_ground_station, delete_ground_station, fetch_all_ground_stations,
+        fetch_ground_station,
+    },
+    jobs::create_job,
+    satellites::{
+        create_satellite, delete_satellite, fetch_all_satellites, fetch_satellite,
+        update_satellite_tle,
+    },
+    telemetry::fetch_satellite_telemetry,
+};
+
+/// Configure all application routes
+pub fn configure_routes(cfg: &mut web::ServiceConfig) {
+    cfg
+        // Telemetry
+        .service(fetch_satellite_telemetry)
+        // Config & Control
+        .service(get_config)
+        .service(send_command)
+        // Ground Stations
+        .service(create_ground_station)
+        .service(fetch_all_ground_stations)
+        .service(fetch_ground_station)
+        .service(delete_ground_station)
+        // Jobs
+        .service(create_job)
+        // Satellites
+        .service(fetch_all_satellites)
+        .service(fetch_satellite)
+        .service(create_satellite)
+        .service(update_satellite_tle)
+        .service(delete_satellite);
+}
